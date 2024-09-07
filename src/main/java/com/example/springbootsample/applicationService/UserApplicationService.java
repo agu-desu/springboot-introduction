35185185b7df5d4f1fdcde0ce75a606a3e307304
“羽賀阿矩” <haga_aku@plise.co.jp>
2024-09-08 03:16:37 +0900

    first commit(5章まで)

---
+++ src/main/java/com/example/springbootsample/applicationService/UserApplicationService.java
@@ -0,0 +1,21 @@
+package com.example.springbootsample.applicationService;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserApplicationService {
+    
+    /**
+     * 性別のMapを生成する
+     * @return　genderMap 性別のマップ
+     */
+    public Map<String, Integer> getGenderMap() {
+        Map<String, Integer> genderMap = new LinkedHashMap<>();
+        genderMap.put("男性", 1);
+        genderMap.put("女性", 2);
+        return genderMap;
+    }
+}
---
+++ src/main/java/com/example/springbootsample/hello/HelloService.java
@@ -0,0 +1,36 @@
+package com.example.springbootsample.hello;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+    
+    @Autowired
+    private HelloRepository repository;
+    
+    /**
+     * 従業員を一人取得する
+     * @param String id
+     * @return Employee  employee
+     */
+    public Employee getEmployee(String id) {
+        // 検索
+        Map<String, Object> map = repository.findById(id);
+
+        // Mapから値を取得
+        String employeeId = (String) map.get("id");
+        String name = (String) map.get("name");
+        int age = (Integer) map.get("age");
+
+        // Employeeクラスに値をセット
+        Employee employee = new Employee();
+        employee.setEmployeeId(employeeId);
+        employee.setEmployeeName(name);
+        employee.setEmployeeAge(age);
+
+        return employee;
+    }
+}
