f005fc5d012a9155397e172065b0399a52347cb9
“羽賀阿矩” <haga_aku@plise.co.jp>
2024-09-09 00:19:21 +0900

    ６章実装完了

--- src/main/java/com/example/springbootsample/controller/SignupController.java
+++ src/main/java/com/example/springbootsample/controller/SignupController.java
@@ -7,9 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.example.springbootsample.applicationService.UserApplicationService;
+import com.example.springbootsample.form.GroupOrder;
+import com.example.springbootsample.form.SignupForm;
+
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
+@Slf4j // 簡単にログが出力できる。
 public class SignupController {
     
     @Autowired
@@ -27,7 +35,7 @@ public class SignupController {
      * 
      */
     @GetMapping("/signup")
-    public String getSignup(Model model, Locale locale) {
+    public String getSignup(Model model, Locale locale, @ModelAttribute SignupForm form) {
         // 性別を取得
         Map<String, Integer> genderMap = userApplicationService.getGenderMap(locale);
         model.addAttribute("genderMap", genderMap);
@@ -39,8 +47,15 @@ public class SignupController {
      * ユーザー登録処理
      */
     @PostMapping("/signup")
-    public String postSignup() {
+    public String postSignup(Model model, Locale locale, @ModelAttribute @Validated(GroupOrder.class)SignupForm form,
+    BindingResult bindingResult) {
+        // 入力チェック
+        if(bindingResult.hasErrors()) {
+            // NG: ユーザー登録画面に戻ります
+            return getSignup(model, locale, form);
+        }
         // ログイン画面にリダイレクト
+        log.info(form.toString());
         return "redirect:/login";
     }
     
