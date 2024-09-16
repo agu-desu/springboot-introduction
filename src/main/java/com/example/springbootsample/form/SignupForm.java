11eccd4511ca15181e0271c08a9f6543566445b8
“羽賀阿矩” <haga_aku@plise.co.jp>
2024-09-16 17:10:13 +0900

    ６章正規表現ミス　修正

--- src/main/java/com/example/springbootsample/form/SignupForm.java
+++ src/main/java/com/example/springbootsample/form/SignupForm.java
@@ -23,9 +23,7 @@ public class SignupForm {
 
     @NotBlank(groups = ValidGroup1.class)
     @Length(min = 4, max = 100, groups = ValidGroup2.class)
-    @Pattern(regexp = "[a-zA-Z0-9] + $", groups = ValidGroup2.class)
-
-    @NotBlank(groups = ValidGroup1.class)
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", groups = ValidGroup2.class)
     private String password;
 
     @NotBlank(groups = ValidGroup1.class)
